d815de490ba8743a772be0ff084e045ebac42b98
guipateiro <57327655+guipateiro@users.noreply.github.com>
2021-02-08 15:33:56 -0300

    Add files via upload

---
+++ hash.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <limits.h>
+#include <stdlib.h>
+#define TAM 11
+
+typedef struct {
+	int elemento;
+	int excluido;
+} hash;
+
+typedef struct{
+	int elemento;
+	char lista[3];
+	char endereco;
+} resposta;
+
+void iniciatabela(hash *tabela){
+	int temp;
+
+	for(temp = 0 ; temp <= TAM ; temp++){
+		tabela[temp].excluido = 1;
+		tabela[temp].elemento = 0;
+	}
+}
+
+void imprime_tabelas(hash *tabela1, hash *tabela2){
+	int temp;
+	hash t1temp = {INT_MAX,1};
+	hash t2temp = {INT_MAX,1};
+
+	for(temp = 0 ; temp <= TAM ; temp++){
+		if((tabela1[temp].excluido == 0) && (tabela1[temp].elemento <= t1temp.elemento)){
+			tabela1[temp] = t1temp;
+		}
+		if((tabela2[temp].excluido == 0) && (tabela2[temp].elemento <= t2temp.elemento)){ 
+			tabela2[temp] = t2temp;
+		}
+
+	}
+}	
+
+
+int main(){
+	hash tabela1[TAM];
+	hash tabela2[TAM];
+	int i;
+	char c;
+	iniciatabela(tabela1);
+	iniciatabela(tabela2);
+
+	if(scanf("%c %i", &c, &i) != 2){
+		fprintf(stderr,"Entrada inválida");
+        exit(1);
+	}
+	while(scanf("%c %i", &c, &i) == 2 && !feof(stdin)){
+		if (c == 'i')
+			insere(i, tabela1, tabela2);
+		else if	(c == 'r')
+			exclui(i, tabela1, tabela2);
+	}
+	
+	imprime_tabelas(tabela1,tabela2);
+
+	return(0);
+}
